9182b0c7d1264c149156a1609da64cacd8ff509a
James Rickelton <jjarickelton@gmail.com>
2021-04-19 15:27:19 +0100

    Update users schema to include timestamp

--- Backend/Mongoose-test-scenarios/schema/users-schema.js
+++ Backend/Mongoose-test-scenarios/schema/users-schema.js
@@ -1,15 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const UserSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  isOver18: Boolean,
-  email: { type: String, required: true, unique: true },
-  blurbles: Number,
-  badge: Array,
-  avatar: String,
-  clubs: [{ clubName: String, club_Id: String, progress: Number }]
-});
+const UserSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    isOver18: Boolean,
+    email: { type: String, required: true, unique: true },
+    blurbles: Number,
+    badge: Array,
+    avatar: String,
+    clubs: [{ clubName: String, club_Id: String, progress: Number }],
+
+    //location - possibly
+  },
+  { timestamps: { createdAt: "created_at" } }
+);
 
 module.exports = UserSchema;
