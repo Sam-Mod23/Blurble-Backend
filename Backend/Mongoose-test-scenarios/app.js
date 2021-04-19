f489662273d2a4aa5ff732fd7e44273b33c3bbf3
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-19 16:56:26 +0100

    extracted out seed functions

--- Backend/Mongoose-test-scenarios/app.js
+++ Backend/Mongoose-test-scenarios/app.js
@@ -1,48 +1,40 @@
-const mongoose = require("mongoose");
-const UserSchema = require("./schema/users-schema");
-const ClubSchema = require("./schema/clubs-schema");
+const mongoose = require('mongoose');
+const seedDb = require('./seed');
+// const UserSchema = require('./schema/users-schema');
+// const ClubSchema = require('./schema/clubs-schema');
 
-mongoose.connect("mongodb://localhost:27017/blurble", {
+mongoose.connect('mongodb://localhost:27017/blurble', {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
 
-const User = mongoose.model("User", UserSchema);
-const Club = mongoose.model("Club", ClubSchema);
+// const User = mongoose.model('User', UserSchema);
+// const Club = mongoose.model('Club', ClubSchema);
 
-db.on("error", console.error.bind(console, "connection error"));
-db.once("open", () => {
-  mongoose.connection.db.listCollections().toArray((err, data) => {
-    console.log(data);
-  });
+db.on('error', console.error.bind(console, 'connection error'));
+db.once('open', () => {
+  db.dropDatabase()
+    .then((err) => {
+      if (err) console.log(err);
+      console.log('Database dropped');
+    })
+    .then(() => {
+      seedDb();
+    });
+  // .then(() => {
+  //   mongoose.connection.db.listCollections().toArray((err, data) => {
+  //     console.log(err, data, 'in collection list');
+  //   });
+  // });
 
-  User.find(function (err, user) {
-    if (err) return console.error(err);
-    console.log(user);
-  });
+  //   User.find(function (err, user) {
+  //     if (err) return console.error(err);
+  //     console.log(user);
+  //   });
 });
 
-User.insertMany(
-  [{ username: "user1", name: "Sam", isOver18: false, email: "email1" }],
-  (err, data) => {
-    console.log(err);
-  }
-);
-Club.insertMany(
-  [
-    {
-      clubName: "Blurble Club",
-      description: "test",
-      currentBook: { book: "test" },
-      memberIds: [1],
-      adminIds: [1],
-    },
-  ],
-  (err, data) => {}
-);
-
-mongoose.set("useCreateIndex", true);
+mongoose.set('useCreateIndex', true);
 
 module.exports = db;
