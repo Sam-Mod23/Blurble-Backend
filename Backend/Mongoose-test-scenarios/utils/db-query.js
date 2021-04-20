e9b529b5c7325e6984dd71bd3b8debe81a2ef83f
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-20 11:59:40 +0100

    create file to successfully query db

--- Backend/Mongoose-test-scenarios/utils/db-query.js
+++ Backend/Mongoose-test-scenarios/utils/db-query.js
@@ -1,2 +1,16 @@
-const mongoose = require('mongoose');
-const db = require('../app');
+const { db, User, Club, Comment } = require('../app');
+
+User.find({}, function (err, users) {
+  if (err) return console.error(err);
+  console.log(users, '---find');
+});
+
+Club.find({}, function (err, clubs) {
+  if (err) return console.error(err);
+  console.log(clubs, '---find');
+});
+
+Comment.find({}, function (err, comments) {
+  if (err) return console.error(err);
+  console.log(comments, '---find');
+});
