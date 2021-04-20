f0e5c6f31f55d8c32233fdb2500a958570eb4efb
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-20 17:24:58 +0100

    update console logs to specify findings

--- Backend/Mongoose-test-scenarios/utils/db-query.js
+++ Backend/Mongoose-test-scenarios/utils/db-query.js
@@ -2,15 +2,15 @@ const { db, User, Club, Comment } = require('../app');
 
 User.find({}, function (err, users) {
   if (err) return console.error(err);
-  console.log(users, '---find');
+  console.log(users, '---users');
 });
 
 Club.find({}, function (err, clubs) {
   if (err) return console.error(err);
-  console.log(clubs, '---find');
+  console.log(clubs, '---clubs');
 });
 
 Comment.find({}, function (err, comments) {
   if (err) return console.error(err);
-  console.log(comments, '---find');
+  console.log(comments, '---comments');
 });
