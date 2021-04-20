58eddf3d4bb0932fefc24761aab5137185e3ee46
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-20 11:57:04 +0100

    centralise mongoose methods for schema creation

--- Backend/Mongoose-test-scenarios/app.js
+++ Backend/Mongoose-test-scenarios/app.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-const seedDb = require('./seed');
-// const UserSchema = require('./schema/users-schema');
-// const ClubSchema = require('./schema/clubs-schema');
+const UserSchema = require('./schema/users-schema');
+const ClubSchema = require('./schema/clubs-schema');
+const CommentSchema = require('./schema/comments-schema');
 
 mongoose.connect('mongodb://localhost:27017/blurble', {
   useNewUrlParser: true,
@@ -10,31 +10,15 @@ mongoose.connect('mongodb://localhost:27017/blurble', {
 
 const db = mongoose.connection;
 
-// const User = mongoose.model('User', UserSchema);
-// const Club = mongoose.model('Club', ClubSchema);
-
 db.on('error', console.error.bind(console, 'connection error'));
 db.once('open', () => {
-  db.dropDatabase()
-    .then((err) => {
-      if (err) console.log(err);
-      console.log('Database dropped');
-    })
-    .then(() => {
-      seedDb();
-    });
-  // .then(() => {
-  //   mongoose.connection.db.listCollections().toArray((err, data) => {
-  //     console.log(err, data, 'in collection list');
-  //   });
-  // });
-
-  //   User.find(function (err, user) {
-  //     if (err) return console.error(err);
-  //     console.log(user);
-  //   });
+  console.log('Connected to db');
 });
 
 mongoose.set('useCreateIndex', true);
 
-module.exports = db;
+const User = mongoose.model('User', UserSchema);
+const Club = mongoose.model('Club', ClubSchema);
+const Comment = mongoose.model('Comment', CommentSchema);
+
+module.exports = { db, User, Club, Comment };
