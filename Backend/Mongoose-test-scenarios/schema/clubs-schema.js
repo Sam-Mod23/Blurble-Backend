35bbfe7f5d3e28daee4cec13b6693bb308951f58
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-20 17:24:25 +0100

    restore clubs schema to include all fields

--- Backend/Mongoose-test-scenarios/schema/clubs-schema.js
+++ Backend/Mongoose-test-scenarios/schema/clubs-schema.js
@@ -1,6 +1,19 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const ClubSchema = new Schema({}, { timestamps: { createdAt: "created_at" } });
+const ClubSchema = new Schema(
+  {
+    clubName: { type: String, required: true, unique: true },
+    description: { type: String, required: true },
+    currentBook: { type: Object, required: true },
+    nominatedBooks: Array,
+    memberIds: { type: Array, required: true },
+    adminIds: { type: Array, required: true },
+    thumbnail: String,
+    comments: Array,
+    archivedBooks: Array
+  },
+  { timestamps: { createdAt: 'created_at' } }
+);
 
 module.exports = ClubSchema;
