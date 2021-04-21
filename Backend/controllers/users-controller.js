9cf61f515eba2b5328b7edacaa527c77c54da042
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-21 13:06:06 +0100

    add patchUserById

--- Backend/controllers/users-controller.js
+++ Backend/controllers/users-controller.js
@@ -2,9 +2,23 @@ const { fetchUserById } = require('../models/users-model');
 
 exports.getUserById = (req, res, next) => {
   const { user_id } = req.params;
+
   fetchUserById(user_id)
     .then((user) => {
       res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.patchUserById = (req, res, next) => {
+  const { user_id } = req.params;
+  const { blurbleInc, club_id, progress } = req.body;
+
+  amendUserById(user_id, blurbleInc)
+    .then((user) => {
+      res.status(201).send(user);
     })
     .catch((err) => {
       console.log(err);
