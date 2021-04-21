427aafc528d08e44e31d51fdf49ac1cc125bfb21
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-21 12:14:32 +0100

    complete user controller for getUserById

---
+++ Backend/controllers/users-controller.js
@@ -0,0 +1,12 @@
+const { fetchUserById } = require('../models/users-model');
+
+exports.getUserById = (req, res, next) => {
+  const { user_id } = req.params;
+  fetchUserById(user_id)
+    .then((user) => {
+      res.status(200).send(user);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
