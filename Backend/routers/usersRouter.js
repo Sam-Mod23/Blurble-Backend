6da34f6ae7ce7a19906af49220dc8bb50f3978cf
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-21 12:15:29 +0100

    require in getUserById

--- Backend/routers/usersRouter.js
+++ Backend/routers/usersRouter.js
@@ -1,3 +1,6 @@
 const usersRouter = require('express').Router();
+const { getUserById } = require('../controllers/users-controller');
+
+usersRouter.route('/:user_id').get(getUserById);
 
 module.exports = usersRouter;
