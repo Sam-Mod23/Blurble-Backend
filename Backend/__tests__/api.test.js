7d2b711e91a33563f4888e541f1b0a78de8701d7
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-22 14:25:09 +0100

    begin testing

--- Backend/__tests__/api.test.js
+++ Backend/__tests__/api.test.js
@@ -1,20 +1,30 @@
-process.env.NODE_ENV = "test";
+// process.env.NODE_ENV = 'test';
+const mongoose = require('mongoose');
 
-const app = require("../app");
-const db = require("../database/db-connection");
-const request = require("supertest");
-const { seedDb } = require("../database/seed");
+const app = require('../app');
+const db = require('../database/db-connection');
+const request = require('supertest');
+const { seedFunction } = require('../database/seed');
+const { beforeEach, expect } = require('@jest/globals');
 
-beforeEach(() => {
-  seedDb();
+beforeEach((done) => {
+  seedFunction().then(() => {
+    console.log('beforeEach');
+    done();
+  });
 });
 
 afterAll(() => {
-  db.close();
+  console.log('afterAll');
 });
 
-describe("Name of the group", () => {
-  test("should ", () => {
-    console.log("testing");
+describe('Name of the group', () => {
+  test('should ', () => {
+    return request(app)
+      .get('/api/users/6080273f57b6c0468732f97f')
+      .expect(200)
+      .then((res) => {
+        expect(res.body.length).toEqual(5);
+      });
   });
 });
