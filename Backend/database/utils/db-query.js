94ff393b06ce25a267cf356aa1d0c8c49be747f1
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-23 11:05:21 +0100

    introduce query script for test data

--- Backend/database/utils/db-query.js
+++ Backend/database/utils/db-query.js
@@ -1,16 +1,16 @@
-const { db, User, Club, Comment } = require('../db-connection.js');
+const { db, User, Club, Comment } = require("../db-connection.js");
 
 User.find({}, function (err, users) {
   if (err) return console.error(err);
-  console.log(users, '---users');
+  console.log(users, "---users");
 });
 
 Club.find({}, function (err, clubs) {
   if (err) return console.error(err);
-  console.log(clubs, '---clubs');
+  console.log(clubs, "---clubs");
 });
 
 Comment.find({}, function (err, comments) {
   if (err) return console.error(err);
-  console.log(comments, '---comments');
+  console.log(comments, "---comments");
 });
