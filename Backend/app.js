ad66c021c372169a348873bddaf7afe3e03ff482
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-25 16:56:31 +0100

    add getAllEndpoints to app

--- Backend/app.js
+++ Backend/app.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const apiRouter = require('./routers/apiRouter');
+const apiRouter = require("./routers/apiRouter");
+const { errorMongo, error404 } = require("./errors");
 
 app.use(express.json());
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
-// app.all('/*', error404);
+app.use(errorMongo);
+
+app.all("/*", error404);
 
 module.exports = app;
