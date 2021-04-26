5b6a1fa5aeaf020fa51b666a0b15c966a41d1fc1
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-26 19:54:37 +0100

    add post club method

--- routers/clubsRouter.js
+++ routers/clubsRouter.js
@@ -2,7 +2,7 @@ const clubsRouter = require("express").Router();
 const { getClubs, getClub } = require("../controllers/clubs-controller");
 const { error405 } = require("../errors");
 
-clubsRouter.route("/").get(getClubs).all(error405);
+clubsRouter.route("/").get(getClubs).post(postClub).all(error405);
 clubsRouter.route("/_id=:_id").get(getClub).all(error405);
 clubsRouter.route("/clubName=:clubName").get(getClub).all(error405);
 
