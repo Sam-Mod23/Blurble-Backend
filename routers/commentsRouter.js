ad39f865ed72f98c946f7a6f105b550a27edccc4
James Rickelton <jjarickelton@gmail.com>
2021-04-26 22:58:53 +0100

    test GET comments by club_name

--- routers/commentsRouter.js
+++ routers/commentsRouter.js
@@ -2,5 +2,6 @@ const commentsRouter = require("express").Router();
 const { getComments } = require("../controllers/comments-controller");
 
 commentsRouter.route("/club_id=:club_id").get(getComments);
+commentsRouter.route("/club_name=:club_name").get(getComments);
 
 module.exports = commentsRouter;
