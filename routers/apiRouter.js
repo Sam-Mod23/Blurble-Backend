1417af7caf751dfa402bc36d76ae7580f7ed65e1
Sam Modder-Hughes <sjmodderhug@gmail.com>
2021-04-26 18:35:27 +0100

    add clubs endpoints

--- routers/apiRouter.js
+++ routers/apiRouter.js
@@ -2,12 +2,12 @@ const apiRouter = require("express").Router();
 const { error405 } = require("../errors");
 const { getAllEndpoints } = require("../controllers/api-controller");
 const usersRouter = require("./usersRouter");
-// const clubsRouter = require('./clubsRouter');
+const clubsRouter = require("./clubsRouter");
 
 apiRouter.route("/").get(getAllEndpoints).all(error405);
 
 apiRouter.use("/users", usersRouter);
-// apiRouter.use('/clubs', clubsRouter);
+apiRouter.use("/clubs", clubsRouter);
 // apiRouter.use('/books', booksRouter);
 
 module.exports = apiRouter;
