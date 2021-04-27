66d12805664a95d366efd04f6b7282c99a2e9e1d
James Rickelton <jjarickelton@gmail.com>
2021-04-27 13:36:17 +0100

    error handling endpoint

--- controllers/comments-controller.js
+++ controllers/comments-controller.js
@@ -1,7 +1,11 @@
 const { fetchComments } = require("../models/comments-model");
 
 exports.getComments = (req, res, next) => {
-  fetchComments(req.params).then((comments) => {
-    res.status(200).send({ comments });
-  });
+  fetchComments(req.params)
+    .then((comments) => {
+      res.status(200).send({ comments });
+    })
+    .catch((err) => {
+      next(err);
+    });
 };
